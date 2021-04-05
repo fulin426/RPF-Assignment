53143d91956523617f1002658520712b314c37d6
fulinliu <fulin426@gmail.com>
2021-04-05 14:14:01 -0700

    added helper functions for collapse logic

--- scripts.js
+++ scripts.js
@@ -1,51 +1,37 @@
 const intersection = document.querySelector(".intersection");
 const approaches = document.querySelector(".approaches-container");
 const intersectionPlusIcon = document.querySelector(".intersection-plus");
-const departures = document.querySelectorAll(".departures-container")
+const departures = document.querySelectorAll(".departures-container");
+const approachChildren = document.querySelectorAll(".approach-children");
 
 // intersection logic
 intersection.addEventListener("click", function() {
   toggle(intersection, "intersection-selected");
   toggle(approaches, "hide");
+
+  if (!intersection.classList.contains("intersection-selected")) {
+    intersectionPlusIcon.classList.remove("intersection-plus-selected");
+    collapseAllIntersections();
+    const text = intersectionPlusIcon.firstChild;
+    text.nodeValue = '++';
+  }
 });
 
 intersectionPlusIcon.addEventListener("click", function() {
   toggle(intersectionPlusIcon, "intersection-plus-selected");
-  const text = intersectionPlusIcon.firstChild
+  const text = intersectionPlusIcon.firstChild;
+
   if (text.nodeValue === '++') {
     approaches.classList.remove("hide");
     intersection.classList.add("intersection-selected");
-
-    document.querySelectorAll(".approach-children").forEach(function(approach) {
-      if (!approach.classList.contains("approach-selected")) {
-        approach.classList.add("approach-selected");
-      }
-    });
-
-    document.querySelectorAll(".departures-container").forEach(function(depature) {
-      if (depature.classList.contains("hide")) {
-        depature.classList.remove("hide");
-      }
-    });
-
-    text.nodeValue = "--";
+    expandAllIntersections();
   } else {
     approaches.classList.add("hide");
     intersection.classList.remove("intersection-selected");
-    document.querySelectorAll(".approach-children").forEach(function(approach) {
-      if (approach.classList.contains("approach-selected")) {
-        approach.classList.remove("approach-selected");
-      }
-    });
-
-    document.querySelectorAll(".departures-container").forEach(function(depature) {
-      if (!depature.classList.contains("hide")) {
-        depature.classList.add("hide");
-      }
-    });
-
-    text.nodeValue = "++";
+    collapseAllIntersections();
   }
+
+  text.nodeValue = text.nodeValue === '++' ? "--" : "++";
 });
 
 // logic for approaches
@@ -63,11 +49,39 @@ document.querySelectorAll(".approach").forEach(function(item) {
   });
 });
 
-// Helper functions
+// Helper function for toggle
 function toggle(elem, style) {
   if (elem.classList.contains(style)) {
-    elem.classList.remove(style)
+    elem.classList.remove(style);
   } else {
     elem.classList.add(style);
   }
+}
+
+function expandAllIntersections() {
+  document.querySelectorAll(".approach-children").forEach(function(approach) {
+    if (!approach.classList.contains("approach-selected")) {
+      approach.classList.add("approach-selected");
+    }
+  });
+
+  document.querySelectorAll(".departures-container").forEach(function(depature) {
+    if (depature.classList.contains("hide")) {
+      depature.classList.remove("hide");
+    }
+  });
+}
+
+function collapseAllIntersections() {
+  document.querySelectorAll(".approach-children").forEach(function(approach) {
+    if (approach.classList.contains("approach-selected")) {
+      approach.classList.remove("approach-selected");
+    }
+  });
+
+  document.querySelectorAll(".departures-container").forEach(function(depature) {
+    if (!depature.classList.contains("hide")) {
+      depature.classList.add("hide");
+    }
+  });
 }
